0383dbe07975b874f6ac3779bdfb822bdf4a7f9d
Jérôme Arzel <jerome.arzel@gnuside.com>
2014-07-30 11:34:01 +0200

    Indenting and moving nested functions outside.

--- admin_collapse_subpages.php
+++ admin_collapse_subpages.php
@@ -18,45 +18,42 @@
 if (!class_exists('Admin_Collapse_Subpages')) {
 
 	class Admin_Collapse_Subpages {
-		
-	function __construct() {
-		
-		function acs_scripts(){
-				 global $pagenow;
+		function __construct() {
+			add_action('admin_enqueue_scripts', array($this, 'acs_scripts'));
+		}
 
-					if(isset($_GET['post_type']) ) {				 	
-					 	$post_type = $_GET['post_type'];
-					 	if(is_post_type_hierarchical($post_type)) {
-					 		add_filter( 'admin_body_class', 'acs_admin_body_class' );
-					 	}
+		public function acs_admin_body_class( $classes ) {
+			$classes .= ' ' .'acs-hier';
+			return $classes;
+		}
 
-						function acs_admin_body_class( $classes )
-						{
-						    $classes .= ' ' .'acs-hier';
-						    return $classes;
-						}
+		public function acs_scripts() {
+			global $pagenow;
 
-				 	}
-				 	
-					if ( is_admin() && isset($_GET['post_type']) && $pagenow =='edit.php' ) {
-						
-							//make sure jquery is loaded
-							wp_enqueue_script('jquery');
-							
-							//cookie script for saving collapse states 
-							wp_enqueue_script('jquery-cookie', plugins_url('js/jquery.cookie.js', __FILE__ ), 'jquery', '1.4.0');
-							
-							//main collapse pages script
-							wp_enqueue_script('acs-js',plugins_url('js/admin_collapse_subpages.js', __FILE__ ), false, '2.0');
-						
-							//Load Styles
-							wp_enqueue_style('acs-css', plugins_url('css/style.css', __FILE__ ), false, '2.0', 'screen');
-							}
-					}
-				 add_action('admin_enqueue_scripts','acs_scripts');
+			if(isset($_GET['post_type']) ) {
+				$post_type = $_GET['post_type'];
+				if(is_post_type_hierarchical($post_type)) {
+					add_filter( 'admin_body_class', array($this, 'acs_admin_body_class') );
+				}
 			}
+
+			if ( is_admin() && isset($_GET['post_type']) && $pagenow =='edit.php' ) {
+
+				//make sure jquery is loaded
+				wp_enqueue_script('jquery');
+
+				//cookie script for saving collapse states 
+				wp_enqueue_script('jquery-cookie', plugins_url('js/jquery.cookie.js', __FILE__ ), 'jquery', '1.4.0');
+
+				//main collapse pages script
+				wp_enqueue_script('acs-js',plugins_url('js/admin_collapse_subpages.js', __FILE__ ), false, '2.0');
+
+				//Load Styles
+				wp_enqueue_style('acs-css', plugins_url('css/style.css', __FILE__ ), false, '2.0', 'screen');
+			}
+		}
 	}
-	
+
 	global $collapsePages;
 	$collapsePages = new Admin_Collapse_Subpages();
 }
